c90332ed6b95b21f2dd1410d1d2bd1cce0aed651
samv2791 <verd0059@hz.nl>
2025-02-09 20:10:34 +0100

    added website

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class PagesController extends Controller
+{
+    public function showBlog(): View {
+        return view('blog');
+    }
+
+    public function showBlogPage($id): View {
+        if($id > 0 && $id < 6) {
+        return view('blogPosts', ['id' => $id]);
+        } else {
+            abort(404);
+        }
+    }
+
+    public function showDashboard(): View {
+        return view('dashboard');
+    }
+
+    public function showFaq(): View {
+        return view('faq');
+    }
+
+    public function showIndex(): View {
+        return view('index');
+    }
+
+    public function showProfile(): View {
+        return view('profile');
+    }
+}
